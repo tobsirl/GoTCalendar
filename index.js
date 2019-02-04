64ddbc44a6ea8fcdc39954cc0977d26caac0ff28
Paul Tobin <tobster111@gmail.com>
2019-02-04 15:03:25 +0000

    Dynamically added the image and the text

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const
+const calendarButton = document.querySelector('.btn-start');
+const calendarContainer = document.querySelector('.container');
+
+const calendarDays = 18;
+
+const createCalendar = () => {
+  for (let i = 0; i < calendarDays; i++) {
+    const calendarDoor = document.createElement('div');
+    const calendarDoorText = document.createElement('div');
+
+    calendarDoor.classList.add('image');
+    calendarDoor.style.gridArea = 'door' + (i + 1);
+    calendarContainer.appendChild(calendarDoor);
+
+    calendarDoorText.classList.add('text');
+    calendarDoorText.innerHTML = i + 1;
+    calendarDoor.appendChild(calendarDoorText);
+  }
+};
+
+calendarButton.addEventListener('click', createCalendar);
