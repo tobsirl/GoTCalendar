1b67f283a032228d2d2e84feb267e89cff8d5f70
Paul Tobin <tobster111@gmail.com>
2019-02-04 15:19:04 +0000

    Added a transition background color and faded out the number

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const calendarContainer = document.querySelector('.container');
 
 const calendarDays = 18;
 
+const openDoor = (path, event) => {
+  event.target.parentNode.style.backgroundImage = `url(${path})`;
+  event.target.style.opacity = '0';
+  event.target.style.backgroundColor = '#8ed6ee';
+};
+
 const createCalendar = () => {
   for (let i = 0; i < calendarDays; i++) {
     const calendarDoor = document.createElement('div');
@@ -15,6 +21,11 @@ const createCalendar = () => {
     calendarDoorText.classList.add('text');
     calendarDoorText.innerHTML = i + 1;
     calendarDoor.appendChild(calendarDoorText);
+
+    charNumber = i + 1;
+    let carPath = `./img/got${charNumber}.png`;
+
+    calendarDoorText.addEventListener('click', openDoor.bind(null, carPath));
   }
 };
 
